bf44bb76037bf186b5664ec0c076ff806a062670
ChernyshovVadym <vad14@i.ua>
2024-05-06 17:01:14 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 import { useDispatch, useSelector } from "react-redux";
-import { selectContacts, selectLoading, selectError } from "./redux/selectors";
+import { selectLoading, selectError } from "./redux/selectors";
 import { useEffect } from "react";
 import Loader from "../Loader/Loader";
 import { fetchContacts } from "./redux/contactsOps";
 
 const App = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
 
